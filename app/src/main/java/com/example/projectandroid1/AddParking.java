62acbfa8a9bce7ec00900dc082a84eef0f7b7b92
Michael <michaelv000king@gmail.com>
2024-03-28 13:44:56 +0200

    added in AddParking.java + xml option to add a photo from the local storage of the device.

--- app/src/main/java/com/example/projectandroid1/AddParking.java
+++ app/src/main/java/com/example/projectandroid1/AddParking.java
@@ -1,17 +1,24 @@
 package com.example.projectandroid1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 public class AddParking extends AppCompatActivity {
+    ImageView Parking_photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_parking);
+        Parking_photo = findViewById(R.id.imageView3);
+
     }
     public void B_AddParking(View view) {
         Intent intent = new Intent(this, AddParking.class);
@@ -24,5 +31,18 @@ public class AddParking extends AppCompatActivity {
     public void B_Parking(View view) {
         Intent intent = new Intent(this, Parking.class);
         startActivity(intent);
+    }
+    public void AddPhotoFromGal(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,3);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            Parking_photo.setImageURI(selectedImage);
+        }
     }
 }
